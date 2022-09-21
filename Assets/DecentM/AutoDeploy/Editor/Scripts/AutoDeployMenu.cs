5ffcede6db19a08db9ab760aa4ba24f599e75181
DecentM <decentm@decentm.com>
2022-09-21 03:29:03 +0300

    feat: create auto building functionality, only from the menu for now

---
+++ Assets/DecentM/AutoDeploy/Editor/Scripts/AutoDeployMenu.cs
@@ -0,0 +1,121 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRC.SDKBase.Editor.BuildPipeline;
+using VRC.SDKBase.Editor;
+using VRC.Core;
+#endif
+
+namespace DecentM.AutoDeploy
+{
+    public static class AutoDeployMenu
+    {
+#if UNITY_EDITOR
+        [MenuItem("DecentM/AutoDeploy/Publish Using Current Settings")]
+        public static void OnPublishUsingCurrentSettings()
+        {
+            Debug.Log("OnPublishUsingCurrentSettings();");
+
+            if (APIUser.CurrentUser == null)
+            {
+                // TODO: CurrentUser is null if the user is logged in, but isn't looking at the SDK window.
+
+                Debug.LogError($"No user found in the SDK. Did authentication fail perhaps?");
+                return;
+            }
+
+            bool hasPermission = APIUser.CurrentUser.canPublishWorlds;
+
+            if (!hasPermission)
+            {
+                Debug.LogError("The currently logged in user does not have permission to upload worlds.");
+                return;
+            }
+
+            bool buildChecks = VRCBuildPipelineCallbacks.OnVRCSDKBuildRequested(VRCSDKRequestedBuildType.Scene);
+
+            if (!buildChecks)
+            {
+                EditorUtility.DisplayDialog("Build failed", "At least one build check failed. Investigate the log output above to debug the issue, then build again.", "Ok");
+                return;
+            }
+
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+
+            EnvConfig.ConfigurePlayerSettings();
+            EditorPrefs.SetBool("VRC.SDKBase_StripAllShaders", false);
+
+            // "shouldBuildUnityPackage" is called future proof publishing in the SDK UI
+            VRC_SdkBuilder.shouldBuildUnityPackage = false;
+
+            VRC_SdkBuilder.PreBuildBehaviourPackaging();
+            VRC_SdkBuilder.ExportAndUploadSceneBlueprint();
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange change)
+        {
+            switch (change)
+            {
+                // The SDK switched to play mode before starting upload (kinda weird, why not just have all the UI in the inspector?)
+                case PlayModeStateChange.ExitingEditMode:
+                case PlayModeStateChange.EnteredPlayMode:
+                    CreateAndAttachRuntimeObject();
+                    EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+                    break;
+            }
+        }
+
+        private static GameObject tmpObject
+        {
+            get
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                GameObject[] roots = scene.GetRootGameObjects();
+
+                AutoDeployRuntime runtime = null;
+
+                foreach (GameObject root in roots)
+                {
+                    runtime = root.GetComponent<AutoDeployRuntime>();
+
+                    if (runtime != null)
+                        break;
+
+                    runtime = root.GetComponentInChildren<AutoDeployRuntime>();
+
+                    if (runtime != null)
+                        break;
+                }
+
+                if (runtime == null)
+                    return null;
+
+                return runtime.gameObject;
+            }
+        }
+
+        private static void CreateAndAttachRuntimeObject()
+        {
+            if (tmpObject != null)
+                CleanRuntimeObject();
+
+            GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            obj.AddComponent<AutoDeployRuntime>();
+            Component.DestroyImmediate(obj.GetComponent<MeshRenderer>());
+            Component.DestroyImmediate(obj.GetComponent<BoxCollider>());
+            Component.DestroyImmediate(obj.GetComponent<MeshFilter>());
+
+            obj.name = "AutoDeployRuntime";
+        }
+
+        private static void CleanRuntimeObject()
+        {
+            if (tmpObject == null)
+                return;
+
+            GameObject.DestroyImmediate(tmpObject);
+        }
+#endif
+    }
+}
---
+++ Assets/DecentM/AutoDeploy/Scripts/AutoDeployRuntime.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+
+#if COMPILER_UDONSHARP
+using UdonSharp;
+#endif
+
+#if UNITY_EDITOR
+using UnityEngine.SceneManagement;
+using VRC.Core;
+using VRCSDK2;
+#endif
+
+namespace DecentM.AutoDeploy
+{
+    internal enum PipelineReadiness
+    {
+        NotReady,
+        Ready,
+        Errored,
+    }
+
+#if COMPILER_UDONSHARP
+    public class AutoDeployRuntime : UdonSharpBehaviour
+#else
+    public class AutoDeployRuntime : MonoBehaviour
+#endif
+    {
+#if UNITY_EDITOR
+        private const float PrepareTimeoutSeconds = 30f;
+
+        private static PipelineReadiness GetPipelineReadiness()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            PipelineManager pipelineManager = null;
+
+            foreach (GameObject rootObject in scene.GetRootGameObjects())
+            {
+                pipelineManager = rootObject.GetComponentInChildren<PipelineManager>();
+
+                if (pipelineManager != null)
+                    break;
+            }
+
+            if (pipelineManager == null)
+            {
+                Debug.LogError("[DecentM.AutoDeploy] Pipeline manager component not found in active scene.");
+                return PipelineReadiness.Errored;
+            }
+
+            return pipelineManager.completedSDKPipeline ? PipelineReadiness.Ready : PipelineReadiness.NotReady;
+        }
+
+        private static RuntimeWorldCreation GetWorldCreation()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            GameObject[] rootObjects = scene.GetRootGameObjects();
+            RuntimeWorldCreation creation = null;
+
+            foreach (GameObject rootObject in rootObjects)
+            {
+                RuntimeWorldCreation candidate = rootObject.gameObject.GetComponentInChildren<RuntimeWorldCreation>();
+
+                if (candidate != null)
+                {
+                    creation = candidate;
+                    break;
+                }
+            }
+
+            return creation;
+        }
+
+        private static void SubmitUploadForm()
+        {
+            RuntimeWorldCreation creation = GetWorldCreation();
+
+            // TODO: read these from settings
+            creation.blueprintName.text = "AutoDeploy Test World";
+            creation.blueprintDescription.text = "Testing deploying worlds from a CI";
+            creation.worldCapacity.text = "64";
+            creation.contentSex.isOn = false;
+            creation.contentViolence.isOn = false;
+            creation.contentGore.isOn = false;
+            creation.contentOther.isOn = false;
+            creation.releasePublic.isOn = false;
+            creation.contentFeatured.isOn = false;
+            creation.contentSDKExample.isOn = false;
+
+            // Assuming from how the inspector looks at the build stage, the "Toggle Warrant"
+            // checkbox just enables the upload button without calling into any scripts.
+
+            creation.SetupUpload();
+        }
+
+        private bool IsSDKPrepared()
+        {
+            RuntimeWorldCreation creation = GetWorldCreation();
+
+            // Wait for world creation component
+            if (creation == null)
+                return false;
+
+            // Wait for pipeline to bea ready (world settings loaded into the form)
+            PipelineReadiness pipelineReadiness = GetPipelineReadiness();
+
+            switch (pipelineReadiness)
+            {
+                case PipelineReadiness.Errored:
+                    Debug.LogError("[DecentM.AutoDeploy] VRChat SDK pipeline errored, cannot continue building.");
+                    this.enabled = false;
+                    return false;
+
+                case PipelineReadiness.NotReady:
+                    return false;
+
+                case PipelineReadiness.Ready:
+                default:
+                    this.enabled = false;
+                    break;
+            }
+
+            return true;
+        }
+
+        private float elapsed = 0;
+
+        void Update()
+        {
+            if (!IsSDKPrepared())
+            {
+                this.elapsed += Time.deltaTime;
+
+                if (this.elapsed > PrepareTimeoutSeconds)
+                {
+                    this.elapsed = 0;
+                    Debug.LogError($"[DecentM.AutoDeploy] The SDK could not prepare in {PrepareTimeoutSeconds} seconds, timeout.");
+                    this.enabled = false;
+                    return;
+                }
+
+                return;
+            }
+
+            SubmitUploadForm();
+        }
+#endif
+    }
+}
