ac748636525c33cfc02d1e2ed9a40ce08645492c
DecentM <decentm@decentm.com>
2022-09-24 06:00:08 +0300

    feat: create newsroom scene, add textures

---
+++ Assets/World/Scripts/NewsItem.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class NewsItem : UdonSharpBehaviour
+{
+    public string title;
+    public string link;
+    public string[] keywords;
+    public string[] creator;
+    public string video_url;
+    public string description;
+    public string content;
+    public string pubDate;
+    public string image_url;
+    public string source_id;
+    public string[] country;
+    public string[] category;
+    public string language;
+}
---
+++ Assets/World/Scripts/PlayerPhysics.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayerPhysics : UdonSharpBehaviour
+{
+    void Start()
+    {
+        if (Networking.LocalPlayer == null || !Networking.LocalPlayer.IsValid())
+            return;
+
+        VRCPlayerApi player = Networking.LocalPlayer;
+
+        player.SetJumpImpulse(3);
+        player.SetWalkSpeed(2);
+        player.SetRunSpeed(4);
+        player.SetStrafeSpeed(2);
+    }
+}
